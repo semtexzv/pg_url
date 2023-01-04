df8b374c11048e42457c78c3cd9815785d55b464
Michal Hornický <semtexzv@gmail.com>
2023-01-04 11:37:35 +0100

    Enable fedora-37 build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,17 @@
 
 set -exu
 
-OS=$1
-ARCH=$2
-VER=$3
+
+CONTAINER=${1:?Missing container name}
+ARCH=${2:?Missing architecture}
+VER=${3:?Missing Version}
 
 mkdir -p artifacts
 
-docker buildx build --platform="$OS/$ARCH" -f build/ubuntu-focal/Dockerfile -t "pgx-ubuntu-focal-$ARCH" .
+docker buildx build --platform="linux/$ARCH" -f build/${CONTAINER}/Dockerfile -t "pgx-${CONTAINER}-$ARCH" .
 
 docker run \
   -v "$PWD":/home/docker/pkg \
   -v "$PWD/artifacts":/artifacts \
   -e PG_VERSION="$VER" \
-  "pgx-ubuntu-focal-$ARCH" ./scripts/package.sh
+  "pgx-${CONTAINER}-$ARCH" ./scripts/package.sh
